3d184259f46c2f3b60b152075b304bc3ecf22b73
Rafa Porres Molina <rporresm@redhat.com>
2021-01-14 13:00:49 +0100

    added details to commit ranges
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- pr_check.sh
+++ pr_check.sh
@@ -6,9 +6,11 @@ echo GIT_PREVIOUS_COMMIT=$GIT_PREVIOUS_COMMIT
 echo GIT_COMMIT=$GIT_COMMIT
 
 COMMIT_RANGE="$GIT_PREVIOUS_COMMIT...$GIT_COMMIT"
+MANUAL_COMMIT_RANGE="$(git merge-base HEAD remotes/origin/master)...$(git rev-parse HEAD)"
 
 echo "COMMIT_RANGE=$COMMIT_RANGE"
+echo "MANUAL_COMMIT_RANGE=$MANUAL_COMMIT_RANGE"
 
-echo "MANUAL_COMMIT_RANGE=$(git merge-base HEAD remotes/origin/master)...$(git rev-parse HEAD)"
 
 git diff --name-only "$COMMIT_RANGE"
+git diff --name-only "$MANUAL_COMMIT_RANGE"
