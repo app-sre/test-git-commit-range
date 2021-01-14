130605c7c6020c90487d93d146c2e6217088a89d
Rafa Porres Molina <rporresm@redhat.com>
2021-01-14 12:51:28 +0100

    stuff into the scripts
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -7,4 +7,8 @@ echo GIT_COMMIT=$GIT_COMMIT
 
 COMMIT_RANGE="$GIT_PREVIOUS_COMMIT...$GIT_COMMIT"
 
+echo "COMMIT_RANGE=$COMMIT_RANGE"
+
+echo "MANUAL_COMMIT_RANGE=$(git merge-base HEAD master)...$(git rev-parse HEAD)"
+
 git diff --name-only "$COMMIT_RANGE"
